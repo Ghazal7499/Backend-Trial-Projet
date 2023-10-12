724c520a15b029eabb265f87f2daf92e31a6dfa2
Ghazal7499 <ghazal.ashraf12345@gmail.com>
2023-10-12 09:31:30 +0530

    Initial commit

---
+++ demoBackedStarter/src/main/java/com/demoBackedStarter/DemoBackedStarterApplication.java
@@ -0,0 +1,13 @@
+package com.demoBackedStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackedStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackedStarterApplication.class, args);
+	}
+
+}
---
+++ demoBackedStarter/src/test/java/com/demoBackedStarter/DemoBackedStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoBackedStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackedStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
