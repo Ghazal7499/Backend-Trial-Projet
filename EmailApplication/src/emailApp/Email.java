a8844b4ced7bfc94e0449ac10445b0d88a007a48
Ghazal7499 <ghazal.ashraf1234@gmail.com>
2023-10-13 12:59:43 +0530

    1st Commit

---
+++ EmailApplication/src/emailApp/Email.java
@@ -0,0 +1,26 @@
+package emailApp;
+
+public class Email {
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+
+	// Construtor to recieve the first Name and last Name
+	
+	// Ask for the department
+	
+	// Generate a random password
+	
+	// Set the mailbox capacity
+	
+	// Set the alternate email
+	
+	// Change the password
+	
+	
+}
+
